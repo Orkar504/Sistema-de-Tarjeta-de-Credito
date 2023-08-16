1186392f213ea52f736da54fa068eadd4ee463d9
Orkar504 <jose7211963@gmail.com>
2023-08-15 18:33:28 -0600

    Se creo una nueva pantalla de login que mediante el uso de EMPLEADO ID mas su contraseña respectiva pueda ingresar el usuario. Tambien ya se le agrego en la parte de .cshtml todos los elementos correspondientes.

---
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Sistema_de_Tarjeta_de_Credito.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public bool hasData = false;
+        public int EmpleadoId = 0;
+        public string mensaje = "";
+
+        [BindProperty]
+        public Credencial Crendencial { get; set; }
+        public void onGet()
+        {
+
+        }
+       
+        public class Credencial
+        {
+            [Required]
+            public int EmpleadoId { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Sistema_de_Tarjeta_de_Credito.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -27,6 +29,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
