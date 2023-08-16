3b5047e206b2b1c0f48f5c85e2b4c2ebed3cb4fa
Orkar504 <jose7211963@gmail.com>
2023-08-15 18:56:44 -0600

    Se realizo cambios en el Login.cshtml.cs para hacer un esquema de la validación , tambien se agrego la funcionalidad de ingresar inmediatamente con las cookies para no hacer el trabajo tan tedioso

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,5 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
+//Basado en el código de Frank Liu en su canal de youtube : https://www.youtube.com/@FrankLiuSoftware
+
+//Co-author = Orkar504
+//Jose Martinez
+//20191031721
 #nullable disable
 
 using System;
@@ -15,25 +21,60 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Security.Claims;
 
 namespace Sistema_de_Tarjeta_de_Credito.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public bool hasData = false;
-        public int EmpleadoId = 0;
-        public string mensaje = "";
+      
 
         [BindProperty]
-        public Credencial Crendencial { get; set; }
+        public Credencial crendencial { get; set; }
         public void onGet()
         {
 
         }
-       
+
+        public void onPost()
+        {
+    
+            if (!ModelState.IsValid) return;
+
+            // Para la verificación de las credenciales
+            if(crendencial.EmpleadoId == "0000" && crendencial.Password == "contra")
+            {
+                //Creando Contexto para la seguridad
+
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name,"0000"),
+                };
+                var identidad = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identidad);
+
+
+                HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+            }
+
+     
+        }
+
         public class Credencial
         {
             [Required]
+            [Display (Name = "ID del Empleado")]
+            public string EmpleadoId { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+        }
+        public class datos
+        {
+            [Required]
+            [Display(Name = "ID del Empleado")]
             public int EmpleadoId { get; set; }
 
             [Required]
