1ecde1b572de7fa84bd9232d6ff9a739c5a70fb5
yefryfunez <85424939+yefryfunez@users.noreply.github.com>
2023-08-17 22:39:31 -0600

    Add files via upload
    Estado de cuenta agregado, falta todavia modificar algunas cosas

---
+++ Pages/Cuentas/EstadoCuenta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_de_Tarjeta_de_Credito.Pages.Cuentas
+{
+    public class EstadoCuentaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
