531ca5d24590f09e4c3a76f4c49b0e0b15c23e71
Orkar504 <jose7211963@gmail.com>
2023-08-19 11:37:30 -0600

    Empezando a trabajar con las autorizaciones de usuarios, primer, con el usuario de prueba

--- Pages/Prueba1.cshtml.cs
+++ Pages/Prueba1.cshtml.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks
 
 namespace Sistema_de_Tarjeta_de_Credito.Pages
 {
+    [Authorize]
     public class Prueba1Model : PageModel
     {
         public void OnGet()
