a1874bcb2d828c674943065e368b3efc1f0c3f2d
yidsuds <jaramirezc@unah.hn>
2023-08-20 00:58:34 -0600

    autorizacion
    pos hice esto, espero que quede bonito :,3

---
+++ Pages/Autorizacion_de_tarjeta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_de_Tarjeta_de_Credito.Pages
+{
+    public class Autorizacion_de_tarjetaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
