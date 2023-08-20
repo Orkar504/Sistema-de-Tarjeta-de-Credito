698235cb99c725dc7b7ed5e8eef0b87e6f0f17bd
NoheliaHdz <noheliah61@gmail.com>
2023-08-20 04:14:31 -0600

    agregado del frontend de queja y detalle queja siendo el segundo donde podemos ver la informacion completa de la queja

---
+++ Pages/Detalle_Queja.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_de_Tarjeta_de_Credito.Pages
+{
+    public class Detalle_Queja : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Quejas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_de_Tarjeta_de_Credito.Pages
+{
+    public class Quejas : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
